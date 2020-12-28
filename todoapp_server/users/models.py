948c1ed0ed8e5e377de47aa368a084d74247f06c
duca43 <dmaksic@uns.ac.rs>
2020-12-28 18:41:30 +0100

    overriden first_name, last_name and email fields into required fields

--- todoapp_server/users/models.py
+++ todoapp_server/users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
     address = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=50, blank=True)
     country = models.CharField(max_length=50, blank=True)
