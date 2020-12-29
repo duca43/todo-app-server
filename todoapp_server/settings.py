f2df13ea7560262bd19ea9b2b882368d07eddc66
duca43 <dmaksic@uns.ac.rs>
2020-12-29 18:41:47 +0100

    added CORS support

--- todoapp_server/settings.py
+++ todoapp_server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #third-party apps
     'rest_framework',
+    'corsheaders',
 
     #my apps
     'todoapp_server.users',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,10 +132,14 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
 
-#Rest framework
+# Rest framework
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
-} 
+}
+
+# CORS configuration
+
+CORS_ALLOW_ALL_ORIGINS = True # Ovo je samo dok ne budem imao FE
